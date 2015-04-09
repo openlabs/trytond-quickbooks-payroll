934432dee3d0deef08f65487fa602a4efca41058
Priyanka Jain <priyanka.jain@openlabs.co.in>
2015-04-09 16:52:14 +0530

    Add Quickbook Source Name #7369

--- __init__.py
+++ __init__.py
@@ -6,11 +6,14 @@
     :license: BSD, see LICENSE for more details.
 """
 from trytond.pool import Pool
+
 from quickbooks import PayrollAccount
+from employee import Employee
 
 
 def register():
     Pool.register(
         PayrollAccount,
+        Employee,
         module='quickbooks_payroll', type_='model'
     )
